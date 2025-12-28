1a82c787e0920797fda64f55169ad38d49d184aa
Dipangshu Datta <infoforddatta@gmail.com>
2025-12-28 08:38:03 +0530

    Day-1  C++ basics (IO, data types, operators, conditions)

---
+++ 01_cpp_basics/07_switch.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int day;
+    cin >> day;
+    switch (day)
+    {
+    case 1:
+        cout << "Monday";
+        break;
+    
+    default:
+        break;
+    }
+}
